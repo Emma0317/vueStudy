828f01c1642637cb77b24204d90253ccaa345d15
e <mkq@1000da.net>
2017-12-13 18:06:11 +0800

    运用node.js和webpack打包建项目

---
+++ src/components/foot/foot.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="foot">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'foot',
+  data () {
+    return {
+      msg: '共同低部'
+    }
+  }
+}
+</script>
---
+++ src/components/top/top.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="top">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top',
+  data () {
+    return {
+      msg: '共同头部'
+    }
+  }
+}
+</script>
